99180365bd1f248a1d7591317901232634550f10
Dawid <ds127770@stud.ur.edu.pl>
2023-11-06 15:05:33 +0100

    uzupelnienie zadan

--- lab4/lab4.2/contactExcersise/src/contactApp.java
+++ lab4/lab4.2/contactExcersise/src/contactApp.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class contactApp extends JFrame{
     private JPanel JPanel1;
@@ -21,5 +23,20 @@ public class contactApp extends JFrame{
         this.setContentPane(this.JPanel1);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,400);
+        list1.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+            }
+        });
+        createTable();
     }
+
+    private void createTable() {
+        Object[][] data = {
+                {"Jan", "Kowalski", "jkowalski@ur.pl", "323 212 323", 2000-11-10},
+                {"Marian", "Nowak", "mnowak@ur.pl", "323 212 323", 2010-11-10},
+                {"Janina", "Urban", "jurban@ur.pl", "322 123 544", 1996-11-10},
+                {"Ewelina", "Nowak Maciag", "enowakmaciag@ur.pl", "123 767 5656", 2007-11-10},
+        };
 }
